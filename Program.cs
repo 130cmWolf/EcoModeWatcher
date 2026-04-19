c7925af1a175fa48353f7d11e7f6988801d26227
130cmWolf <nagisa.to@gmail.com>
2026-04-20 01:22:07 +0900

    Single instance

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using System.Windows.Forms;
 
+using var mutex = new System.Threading.Mutex(true, "EcoModeWatcher-SingleInstance", out bool createdNew);
+if (!createdNew) return;
+
 Application.EnableVisualStyles();
 Application.SetCompatibleTextRenderingDefault(false);
 
